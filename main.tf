bc53093229dcab18dc50a196cb2342dbcd24a769
Willian Inocencio <willian.i.alves@hotmail.com>
2022-08-02 19:19:49 -0300

    Creating website with s3.

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "site" {
+  bucket = var.site_domain
+}
+
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.site.arn,
+          "${aws_s3_bucket.site.arn}/*",
+        ]
+      },
+    ]
+  })
+}
